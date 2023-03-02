0fde2aeb6624abdf4d039e586d02256905555ae9
_MauroJesus <mauro301002@gmail.com>
2023-03-02 17:12:25 +0100

    Organização de alguns detalhes de nomenclatura

--- src/application/getway/teacher/save.gateway.protocol.ts
+++ src/application/getway/teacher/save.gateway.protocol.ts
@@ -1,6 +1,8 @@
 import { Nivel } from './../../../core/entity/value-object/nivel-academico';
 import { IGenre } from './../../../core/entity/value-object/genre.value-object';
 import { Name } from './../../../core/entity/value-object/name.value-object';
+
+
 export interface IsaveTeacherGateway{
     handle:(parameters:{
         nome:Name,
