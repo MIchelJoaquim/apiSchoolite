994806f16e6e5efaed59d747b72433984864e532
_MauroJesus <mauro301002@gmail.com>
2023-03-03 19:39:02 +0100

    Correçao da nomenclatura da interface

--- src/core/usecase/teacher/edit.usecase.protocol.ts
+++ src/core/usecase/teacher/edit.usecase.protocol.ts
@@ -2,6 +2,8 @@ import { Nivel } from './../../entity/value-object/nivel-academico';
 import { IGenre } from './../../entity/value-object/genre.value-object';
 import { Name } from './../../entity/value-object/name.value-object';
 import { Id } from './../../entity/value-object/id.value-object';
+
+
 export interface IEditTeacher{
     handle(id:Id,props:{
         nome:Name,
