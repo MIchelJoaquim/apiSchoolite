2059bb42d2c1a582ee82c556a582f6a3cd5dfce6
Ramiro Muxili <ramiromarcelo930@gmail.com>
2023-03-06 23:39:12 +0100

    fix:test with jest on name.value-objec, and I disable husky

---
+++ src/core/entity/value-object/__test__/name.value-object.test.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO SE O NOME É UMA STRING VAZIA', () => {
+    it('Step 1', () => {
+        const name = ' ';
+        expect(name).toBe(' ');
+    });
+});
+
+describe('TESTANDO SE O NOME POSSUI PELO MENOS 3 LETRAS', () => {
+    it('Step 2', () => {
+        const name = ('ABC').trim().length;
+        expect(name).toBeLessThanOrEqual(3);
+    });
+});
