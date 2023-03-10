69c6aa11267c67558a10d67d26af167310da11f5
_MauroJesus <mauro301002@gmail.com>
2023-03-10 21:05:19 +0100

    Eliminação do AllMocks e Restruturação das condições no OBJvalor

--- src/core/entity/value-object/__test__/id.value-object.test.ts
+++ src/core/entity/value-object/__test__/id.value-object.test.ts
@@ -2,22 +2,14 @@ import { Id } from './../id.value-object';
 
 
 describe('Id value object',()=>{
-  afterEach(()=> jest.clearAllMocks());
 
   const makeSut = (value: string | number) => {
     return Id.create({ value });
   };
 
-  
   it('Should not create an id negative', ()=>{
     expect(()=> makeSut(-1)).toThrowError("O id não deve ser negativo");
   });
-
-
-  it('Should not have more than 3 characters', () =>{
-    expect(()=>makeSut('abcd')).toThrowError("O id deve ser composto por mais de 3 caracteres")
-  });
-
 
   it('Should return a rigth value after test',()=>{
       const idValue = 1;
