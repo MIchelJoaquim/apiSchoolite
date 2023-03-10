9455889288a3b114b3faaafe7943a0c475d0c826
_MauroJesus <mauro301002@gmail.com>
2023-03-10 15:20:45 +0100

    Correcção dos testes

--- src/core/entity/value-object/__test__/id.value-object.test.ts
+++ src/core/entity/value-object/__test__/id.value-object.test.ts
@@ -8,31 +8,20 @@ describe('Id value object',()=>{
     return Id.create({ value });
   };
 
-  it('Should create a new Id', ()=> {
-      const id = makeSut('1');
-      expect(id).toBeInstanceOf(Id);
+  
+  it('Should not create an id negative', ()=>{
+    expect(()=> makeSut(-1)).toThrowError("O id não deve ser negativo");
   });
 
-  it('Should not create an id negative', ()=>{
-      try {
-        makeSut('-1')
-      } catch (erro:unknown) {
-        expect((erro as Error).message).toBe('O id não deve ser negativo');
-      }
-  });
 
   it('Should not have more than 3 characters', () =>{
-    try {
-      makeSut('abc')
-    } catch (error:unknown) {
-      expect((error as Error).message).toBe('O id deve ser composto por mais de 3 caracteres')
-    }
+    expect(()=>makeSut('abcd')).toThrowError("O id deve ser composto por mais de 3 caracteres")
   });
 
-  it('Should return a rigth value after test',()=>{
-      const idValue= '01';
-      const id = makeSut(idValue);
 
+  it('Should return a rigth value after test',()=>{
+      const idValue = 1;
+      const id = makeSut(idValue);
       expect(id.value).toBe(idValue);
   });
 
