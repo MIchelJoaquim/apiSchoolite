58d2a6bb0dfe5e9c1589574278beccf9b8b70985
Ramiro Muxili <ramiromarcelo930@gmail.com>
2023-03-12 09:45:12 +0100

    Fix: I changed review my code, I use eslint

--- src/core/entity/value-object/__test__/name.value-object.test.ts
+++ src/core/entity/value-object/__test__/name.value-object.test.ts
@@ -1,16 +1,16 @@
-import { Name } from '../name.value-object'
+import { Name } from '../name.value-object';
 
 describe('Name value object.', () => {
   const Sut = (value:string) => {
     return Name.create(value);
-  }
+  };
 
   it('Must not create an empty name', () => {
-    expect(()=> Sut(' ')).toThrowError('O Nome não pode ser uma string vazia')
+    expect(()=> Sut(' ')).toThrowError('O Nome não pode ser uma string vazia');
 });
 
 it('Must not create a name with less than 3 letters', () => {
-  expect(()=> Sut('AA')).toThrowError('O Nome deve possuir pelo menos 3 letras')
+  expect(()=> Sut('AA')).toThrowError('O Nome deve possuir pelo menos 3 letras');
 });
 
  it('should return me rigth value after creation', () => {
@@ -19,7 +19,6 @@ it('Must not create a name with less than 3 letters', () => {
 
   expect(name.value).toBe(nameValue);
 }); 
-
 });
 
   
