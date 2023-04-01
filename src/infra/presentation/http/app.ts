683837a421370fb22d3a0f32c8d72fbc37163f62
MichelJoaquim <majoaquim@cetim.ms>
2023-04-01 01:57:45 +0100

    setup logger

--- src/infra/presentation/http/app.ts
+++ src/infra/presentation/http/app.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import { logResponseTime } from './middleware/responseTimeLogger';
+import { logger } from './helper/logger';
 import routes from './routes';
 
 dotenv.config();
@@ -12,10 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.disable('x-powered-by');
+app.use(logResponseTime);
 app.use('/api/v1', cors({ origin: '*' }), routes);
 
 app.use((_req: Request, res: Response) => {
-  console.log('ROTA NAO ENCONTRADA: ', _req.path);
+  logger.error('Rota não encontrada');
   res.status(404);
   res.send({ msg: 'Lamentamos, o recurso não existe' });
 });
